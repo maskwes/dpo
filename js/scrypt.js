b4d31ac065a990f19753580458fa4d1bb3d1e2e4
Maxim Gerasimov <gersov.maxim.fr@gmail.com>
2021-03-09 16:02:45 +0300

    add mobile menu

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,7 @@
 $(".list__questions__item").click(function(e) {
-    $(this).toggleClass('active-answer');
+    $(this).toggle('active-answer');
+});
+$(".hamburger").click(function(e) {
+    $(this).toggleClass('active-hamburger');
+    $(".mobile-menu").toggleClass('active-menu');
 });
