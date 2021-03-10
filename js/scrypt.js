28d670eddf9c97146c48ae41ad7cfc4442002788
Maxim Gerasimov <gersov.maxim.fr@gmail.com>
2021-03-10 11:33:52 +0300

    add slider

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,7 +1,32 @@
-$(".list__questions__item").click(function(e) {
-    $(this).toggle('active-answer');
-});
-$(".hamburger").click(function(e) {
-    $(this).toggleClass('active-hamburger');
-    $(".mobile-menu").toggleClass('active-menu');
+
+$(document).ready(function(){
+    $(".list__questions__item").click(function(e) {
+        $(this).toggleClass('active-answer');
+    });
+
+    $(".hamburger").click(function(e) {
+        $(this).toggleClass('active-hamburger');
+        $(".mobile-menu").toggleClass('active-menu');
+    });
+
+    $('.slaider-documents').slick({
+
+    });
+
+    $('.wrapper-speakers').slick({
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 2048,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
 });
